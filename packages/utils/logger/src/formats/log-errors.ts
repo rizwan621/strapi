7064b62eee1c055509c64fb28d9a4b9c211d93fb
Alexandre Bodin <bodin.alex@gmail.com>
2024-09-16 10:28:29 +0200

    enhancement: imporve error logging

--- packages/utils/logger/src/formats/log-errors.ts
+++ packages/utils/logger/src/formats/log-errors.ts
@@ -1,8 +1,19 @@
 import { format, Logform } from 'winston';
+import { inspect } from 'util';
+import { pick } from 'lodash';
 
 const logErrors: Logform.FormatWrap = format((info) => {
   if (info instanceof Error) {
-    return { ...info, message: `${info.message as string}${info.stack ? `\n${info.stack}` : ''}` };
+    const { timestamp, message, stack, level, name, ...rest } = info;
+
+    const errorMetas = inspect(pick(rest, Object.keys(rest)), {
+      depth: 1,
+      colors: true,
+    });
+
+    const msg = `${message}${stack ? `\n${stack}` : ''}${errorMetas ? `\n${errorMetas}` : ''}`;
+
+    return { ...info, message: msg };
   }
 
   return info;
